69b7943df9f363db8b2549797c14cc1611a4b3d0
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-12 17:08:20 +0300

    Разобрался немного

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -8,4 +8,16 @@ Console.WriteLine("Введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine ());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine ());
-Console.WriteLine (num1);
+
+
+
+if (num1 > num2)
+{
+    Console.WriteLine ($"Максимальное число: {num1}");   
+}    
+else Console.WriteLine ($"Максимальное число: {num2}");
+// else
+// {
+//    if (num1 < num2);
+//     Console.WriteLine ("Числа равны");
+// }
