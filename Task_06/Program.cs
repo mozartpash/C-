acb25832061c63132e6e252601734bd164561c11
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-13 11:04:17 +0300

    Задание 6 готово

--- Task_06/Program.cs
+++ Task_06/Program.cs
@@ -6,3 +6,11 @@
 // 7 -> нет
 
 
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine ());
+if (num % 2 ==0)
+{  
+    Console.WriteLine ("Число чётное");
+}  
+    
+else Console.WriteLine("Число нечётное");
