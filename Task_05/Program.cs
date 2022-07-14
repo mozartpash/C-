fd94945058e0294b24eeb7406d5a0dd2b6ba2610
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-14 16:14:12 +0300

    Сделал задание 5

--- Task_05/Program.cs
+++ Task_05/Program.cs
@@ -4,3 +4,13 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
 
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int negative = 0 - num;
+Console.Write($"{negative} ,");
+
+while (negative < num)
+{
+    negative = negative + 1;
+    Console.Write($"{negative} ,");
+}
