0384fa2f3c3d3f5e631600b03e5438680c264272
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-14 16:56:42 +0300

    Пытаюсь разобраться с задачей 7

---
+++ Task_07/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("Третья цифра этого числа -> "+stringNumber[2]);
+Console.WriteLine("Введи трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99)
+{
+    Console.Write(num % 10);
+}
+else Console.Write(" Ошибка,введите корректное число");
