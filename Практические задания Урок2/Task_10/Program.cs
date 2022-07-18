9cb29df4e08c708b8d69052048322f40d313883f
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-18 15:21:50 +0300

    Сделал задачу 10

--- Практические задания Урок2/Task_10/Program.cs
+++ Практические задания Урок2/Task_10/Program.cs
@@ -5,3 +5,13 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.WriteLine("Введи трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    num = num / 10;
+    num = num % 10;
+    Console.Write(num);
+}
+else Console.Write("Ошибка");
