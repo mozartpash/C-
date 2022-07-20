3a0a928f7161507d308e2c701ae4753406c3078f
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-07-20 14:15:32 +0300

    Добавил числовой вариант в задаче 13

--- Практические задания Урок2/Task_13/Program.cs
+++ Практические задания Урок2/Task_13/Program.cs
@@ -3,14 +3,27 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("Введи число: ");
+// Console.Write("Введи число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length > 2)
+// {
+//     Console.WriteLine("третья цифра -> " + numText[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// ===================>   Или
+
+
+Console.WriteLine("Введи трёхзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
-string numText = Convert.ToString(num);
-if (numText.Length > 2)
+
+if (num > 99 && num < 1000)
 {
-    Console.WriteLine("третья цифра -> " + numText[2]);
+    num = num % 10;
+    Console.Write(num);
 }
-else
-{
-    Console.WriteLine("Третьей цифры нет");
-}
+else Console.Write("некорректный ввод");
