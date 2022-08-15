4090dd60a5cd1ecd8f54629cb96e00d234253ea1
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2022-08-15 16:56:33 +0300

    added tasks 10,13,15

---
+++ Практические задания Урок2/Task_13/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.Write("Введи число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string numText = Convert.ToString(num);
+// if (numText.Length > 2)
+// {
+//     Console.WriteLine("третья цифра -> " + numText[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// ===================>   Или
+
+
+Console.WriteLine("Введи трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99 && num < 1000)
+{
+    num = num % 10;
+    Console.Write(num);
+}
+else Console.Write("некорректный ввод");
---
+++ Практические задания Урок2/Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введи число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+    case 1 : Console.Write("Понеднльник "); break;
+    case 2 : Console.Write("Вторник "); break;
+    case 3 : Console.Write("Среда "); break;
+    case 4 : Console.Write("Четверг "); break;
+    case 5 : Console.Write("Пятница "); break;
+    case 6 : Console.Write("Суббота - выходной "); break;
+    case 7 : Console.Write("Воскресенье - выходной "); break;
+    default : Console.Write("Нет такого дня недели "); break;
+}
